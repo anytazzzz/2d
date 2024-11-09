a10f4e3f6487ec39b08c1cb4ecc584feb938034d
anytazzzz <igorcernienko30@gmail.com>
2024-11-09 23:41:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+quantity_DZ=12
+quantity_hour=1.5
+course_name="Python"
+Time_for_one=quantity_hour / quantity_DZ
+print("Курс:",course_name,", всего задач:",quantity_DZ,", затарачено часов:",quantity_hour,", среднее время выполнения",Time_for_one,"часа.")
